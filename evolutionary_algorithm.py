2053ccc029e90075c4eeb8871a2a12374885d4bb
FeistKyle <85257085+FeistKyle@users.noreply.github.com>
2021-07-27 16:14:25 -0400

    Update evolutionary_algorithm.py
    Adding code from eff_calc

--- evolutionary_algorithm.py
+++ evolutionary_algorithm.py
@@ -1,5 +1,9 @@
 import subprocess
 import random
+import mcc.lcparquet as lcpq
+import data.dm as dm
+import matplotlib.pyplot as plt
+import dask.array as da
 
 #Run tracking algorithm n times with` different values of z max/min
 
@@ -15,9 +19,27 @@ def marlin_eff(z):
     subprocess.run('source LBLMuCWorkspace/setup.sh', shell=True)
     subprocess.run('Marlin ${MYBUILD}/packages/ACTSTracking/example/actsseed_steer.xml --global.LCIOInputFiles=muonGun_sim_MuColl_v1.slcio', shell=True)
     subprocess.run('source myenv/bin/activate', shell=True)
-    eff = subprocess.run('python eff_calc.py', shell=True, capture_output = True, text = True)
-    print(eff.stdout)
+    
+    #Run eff_calc.py
+
+    data=dm.DataManager('mccplots/data.yaml')  
+    # Load all necessary dataframes
+    samples=['actsseed0']
+    mc=lcpq.concat_load([data.samples[sample] for sample in samples], 'mc')
+    tr=lcpq.concat_load([data.samples[sample] for sample in samples], 'tr')
+    mc2tr=lcpq.concat_load([data.samples[sample] for sample in samples], 'mc2tr')
+    # the single muon
+    mu=mc[mc.colidx==0]
+    # need at least two good hits
+    mc2tr=mc2tr[mc2tr.weight>0.5]
+    # merge to get all info in one dataframe
+    tracks=mu.merge(
+        mc2tr,left_on=['title','evt','colidx'],right_on=['title','evt','from']).merge(
+            tr,left_on=['title','evt','to'],right_on=['title','evt','colidx'])
+
+    eff = len(tracks.index)/len(mu.index)
     return eff
+
     #deactivate, shifter --image gitlab-registry.cern.ch/berkeleylab/muoncollider/muoncollider-docker/mucoll-ilc-framework:1.5.1-centos8 /bin/bash, setup.sh,
     #Marlin ${MYBUILD}/packages/ACTSTracking/example/actsseed_steer.xml --global.LCIOInputFiles=/path/to/events.slcio,source myenv/bin/activate, eff_calc
     
