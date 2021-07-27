4719ff6ab099e31fdb3f4ff67b36083d93a28995
FeistKyle <85257085+FeistKyle@users.noreply.github.com>
2021-07-27 18:53:40 -0400

    Update evolutionary_algorithm.py

--- evolutionary_algorithm.py
+++ evolutionary_algorithm.py
@@ -2,6 +2,12 @@ import subprocess
 import random
 
 
+#Run tracking algorithm n times with` different values of z max/min
+
+#Population size n 
+
+#Function to calculate efficiency
+
 def marlin_eff(z):
     
     shifter ='shifter --image gitlab-registry.cern.ch/berkeleylab/muoncollider/muoncollider-docker/mucoll-ilc-framework:1.5.1-centos8'
@@ -48,5 +54,48 @@ def marlin_eff(z):
     eff = len(tracks.index)/len(mu.index)
     return eff
 
+    #deactivate, shifter --image gitlab-registry.cern.ch/berkeleylab/muoncollider/muoncollider-docker/mucoll-ilc-framework:1.5.1-centos8 /bin/bash, setup.sh,
+    #Marlin ${MYBUILD}/packages/ACTSTracking/example/actsseed_steer.xml --global.LCIOInputFiles=/path/to/events.slcio,source myenv/bin/activate, eff_calc
+    
 
-marlin_eff(4)
+    #calculates eff for a given z
+    # number between 0 and 1 
+
+#Generate z values
+z_values = []
+for z in range(5):
+    z_values.append(random.uniform(0,200))
+
+#Generations 
+for i in range(10):
+
+    #Ranks z values with value for efficiency as a function of z
+    rankedsolutions = []
+    for z in z_values:
+    #z is an element of z_values which was 100 z values generated prior in a for loop
+        rankedsolutions.append((marlin_eff(z), z))
+    rankedsolutions.sort()
+    rankedsolutions.reverse()
+    #At this point there's an ordered list of pairs of values each with an efficiency and a z value for that efficiency from highest to lowest
+    print(f'=== gen {i} best solutions ===')
+    #Prints generation number corresponding to ranked solutions and z values
+    print(rankedsolutions[0])
+    #Prints first efficiency z value pair
+    if rankedsolutions[0][0] > .9:
+        break
+    #Terminates the loop if eff is found to be higher than .9
+    bestsolutions = rankedsolutions[:10]
+    #takes first 10 pairs in ranked solutions
+    elements = []
+    #Creates an empty list called elements
+    for s in bestsolutions:
+        elements.append(s[0][1])
+    #Loops through 10 best solutions, adding the z value of each pair to the elements list
+    newGen = []
+    for _ in range(3):
+        e = random.choice(elements) * random.uniform(0.99, 1.01)
+    #Loops through 100 times, choosing a random z value from elements to mutate
+        newGen.append(e)
+    #Adds mutated elements to newGen which is the new generation 
+    z_values = newGen
+    #newGen becomes the new input for the next iteration i 
