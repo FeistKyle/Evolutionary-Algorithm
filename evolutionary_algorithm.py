cdf272e1e2530afb87bffe3fbba637167a94352a
FeistKyle <85257085+FeistKyle@users.noreply.github.com>
2021-07-27 18:51:30 -0400

    Update evolutionary_algorithm.py

--- evolutionary_algorithm.py
+++ evolutionary_algorithm.py
@@ -2,14 +2,18 @@ import subprocess
 import random
 
 
-#Run tracking algorithm n times with` different values of z max/min
-
-#Population size n 
-
-#Function to calculate efficiency
-
 def marlin_eff(z):
     
+    shifter ='shifter --image gitlab-registry.cern.ch/berkeleylab/muoncollider/muoncollider-docker/mucoll-ilc-framework:1.5.1-centos8'
+    setup ='source LBLMuCWorkspace/setup.sh'
+    marlin ='Marlin ${MYBUILD}/packages/ACTSTracking/example/actsseed_steer.xml --global.LCIOInputFiles=muonGun_sim_MuColl_v1.slcio'
+    venv = 'source myenv/bin/activate'
+    efficiency = 'python eff_calc.py'
+
+    cmd1 = f'{shifter} /bin/bash -c "{setup} && {marlin}"'
+
+    subprocess.run(cmd1)
+
     subprocess.run('deactivate', shell=True)
     subprocess.run('shifter --image gitlab-registry.cern.ch/berkeleylab/muoncollider/muoncollider-docker/mucoll-ilc-framework:1.5.1-centos8 /bin/bash', shell=True)
     subprocess.run('source LBLMuCWorkspace/setup.sh', shell=True)
@@ -20,13 +24,12 @@ def marlin_eff(z):
 
     subprocess.run('Marlin ${MYBUILD}/packages/ACTSTracking/example/actsseed_steer.xml --global.LCIOInputFiles=muonGun_sim_MuColl_v1.slcio', shell=True)
     subprocess.run('source myenv/bin/activate', shell=True)
-    
-    #Run eff_calc.py
+
     import mcc.lcparquet as lcpq
     import data.dm as dm
     import matplotlib.pyplot as plt
     import dask.array as da
-    
+
     data=dm.DataManager('mccplots/data.yaml')  
     # Load all necessary dataframes
     samples=['actsseed0']
@@ -45,48 +48,5 @@ def marlin_eff(z):
     eff = len(tracks.index)/len(mu.index)
     return eff
 
-    #deactivate, shifter --image gitlab-registry.cern.ch/berkeleylab/muoncollider/muoncollider-docker/mucoll-ilc-framework:1.5.1-centos8 /bin/bash, setup.sh,
-    #Marlin ${MYBUILD}/packages/ACTSTracking/example/actsseed_steer.xml --global.LCIOInputFiles=/path/to/events.slcio,source myenv/bin/activate, eff_calc
-    
 
-    #calculates eff for a given z
-    # number between 0 and 1 
-
-#Generate z values
-z_values = []
-for z in range(5):
-    z_values.append(random.uniform(0,200))
-
-#Generations 
-for i in range(10):
-
-    #Ranks z values with value for efficiency as a function of z
-    rankedsolutions = []
-    for z in z_values:
-    #z is an element of z_values which was 100 z values generated prior in a for loop
-        rankedsolutions.append((marlin_eff(z), z))
-    rankedsolutions.sort()
-    rankedsolutions.reverse()
-    #At this point there's an ordered list of pairs of values each with an efficiency and a z value for that efficiency from highest to lowest
-    print(f'=== gen {i} best solutions ===')
-    #Prints generation number corresponding to ranked solutions and z values
-    print(rankedsolutions[0])
-    #Prints first efficiency z value pair
-    if rankedsolutions[0][0] > .9:
-        break
-    #Terminates the loop if eff is found to be higher than .9
-    bestsolutions = rankedsolutions[:10]
-    #takes first 10 pairs in ranked solutions
-    elements = []
-    #Creates an empty list called elements
-    for s in bestsolutions:
-        elements.append(s[0][1])
-    #Loops through 10 best solutions, adding the z value of each pair to the elements list
-    newGen = []
-    for _ in range(3):
-        e = random.choice(elements) * random.uniform(0.99, 1.01)
-    #Loops through 100 times, choosing a random z value from elements to mutate
-        newGen.append(e)
-    #Adds mutated elements to newGen which is the new generation 
-    z_values = newGen
-    #newGen becomes the new input for the next iteration i 
+marlin_eff(4)
